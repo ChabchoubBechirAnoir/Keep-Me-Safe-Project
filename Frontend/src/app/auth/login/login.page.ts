e2657c3d7f8cdd758c686cd73b378744c6d0f83b
Bellakhal Mohamed <mohamed.bellakhal@supcom.tn>
2020-12-26 23:24:28 +0100

    login and signup toasted,reviewed

--- Frontend/src/app/auth/login/login.page.ts
+++ Frontend/src/app/auth/login/login.page.ts
@@ -1,3 +1,4 @@
+import { ToastService } from './../../services/toast.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthConstants } from '../../../config/auth-constants';
@@ -18,43 +19,49 @@ password: ''
 constructor(
 private router: Router,
 private authService: AuthService,
-private storageService: StorageService
-) {}
+private storageService: StorageService,
+private toaster:ToastService) {}
 
 ngOnInit() {}
 
 validateInputs() {
-let username = this.postData.username.trim();
-let password = this.postData.password.trim();
-console.log(username);
-console.log(password);
-return (
-this.postData.username &&
-this.postData.password &&
-username.length > 0 &&
-password.length > 0
-);
+  let username = this.postData.username.trim();
+  let password = this.postData.password.trim();
+  console.log(username);
+  console.log(password);
+  return (
+    this.postData.username &&
+    this.postData.password &&
+    username.length > 0 &&
+    password.length > 0
+  );
 }
 
 loginAction() {
-if (this.validateInputs()) {
-this.authService.login(this.postData).subscribe(
-(res: any) => {
-  console.log(res);
-if (res.token) {
-// Storing the User data.
-this.storageService.store(AuthConstants.AUTH, res.token);
-this.router.navigate(['tabs/tab1']);
-} else {
-console.log('incorrect password.');
-}
-},
-(error: any) => {
-console.log('Network Issue.');
-}
-);
-} else {
-console.log('Please enter email/username or password.');
-}
+  if (this.validateInputs()) {
+    this.authService.login(this.postData).subscribe(
+    (res: any) => {
+      console.log(res);
+    if (res.token) {
+    // Storing the User data.
+      this.storageService.store(AuthConstants.AUTH, res.token);
+      this.router.navigate(['tabs/tab1']); 
+      this.toaster.presentToast('Login successful')
+    } 
+    else {
+      this.toaster.presentToast('incorrect password.')
+      console.log('incorrect password.');
+    }
+  },
+    (error: any) => {
+    this.toaster.presentToast('Invalid credentials.')
+    console.log('Invalid credentials.');
+    }
+    );
+  } 
+  //Missing data in registration
+  else {
+  console.log('Please enter email,username and password.');
+  }
 }
 }
