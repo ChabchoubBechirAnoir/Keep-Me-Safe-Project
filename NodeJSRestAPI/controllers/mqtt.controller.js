72d5c98f674e9200be829f80316435fb05cbc81a
Bechir Anoir Chabchoub <Bechiranoir.chabchoub@expensya.com>
2021-02-06 19:22:17 +0100

    a

--- NodeJSRestAPI/controllers/mqtt.controller.js
+++ NodeJSRestAPI/controllers/mqtt.controller.js
@@ -6,22 +6,8 @@ MqttData.prototype.getResults = async (req, res , next)=> {
     mqttData.find({}, function(err, data) {
       res.json(data);      });
 };
-//Connection to MQTT
-const client = mqtt.connect('mqtts://mqtt.keepmesafe.xyz', {
-  port: 8883,
-  username: 'mqttubuntu',
-  password: '123456789'
-});
-//On received MQTT message
-client.on('message', function (topic, message) {
-    //Saving received data to MongoDB
-    var mongomqttdata = {
-      topic: topic,
-      payload: message.toString()
-    };
-    const saved = await mqttData.createIdentity(mongomqttdata)
-   
-    console.log("a new message is received from your sensors")
-  });
 
+MqttData.prototype.addmqtt = (infos)=> {
+  mqttData.createmqtt(infos);
+};
 module.exports = MqttData;
--- NodeJSRestAPI/routes/mqtts.js
+++ NodeJSRestAPI/routes/mqtts.js
@@ -5,7 +5,22 @@ var MqttData = require('../controllers/mqtt.controller')
 /* GET mqtt listing. */
 
 var mqttData = new MqttData();
-
+//Connection to MQTT
+const client = mqtt.connect('mqtts://mqtt.keepmesafe.xyz', {
+  port: 8883,
+  username: 'mqttubuntu',
+  password: '123456789'
+});
+//On received MQTT message
+client.on('message', function (topic, message) {
+    //Saving received data to MongoDB
+    var mongomqttdata = {
+      topic: topic,
+      payload: message.toString()
+    };
+    const saved = mqttData.addmqtt(mongomqttdata)
+    console.log("a new message is received from your sensors")
+  });
   
 router.get('/',[mqttData.getResults]);
 
